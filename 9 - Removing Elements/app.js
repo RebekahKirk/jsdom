b0105a98dc01f16c3e97c1708307167d5385c009
RebekahKirk <rebekah.kirk@srm.com>
2020-05-06 15:35:37 +0100

    Uploading with challenge 9 added

--- 9 - Removing Elements/app.js
+++ 9 - Removing Elements/app.js
@@ -1,8 +1,10 @@
 const input = document.getElementById('input');
 const button = document.getElementById('submit');
 /*add new const below*/
+const remove = document.getElementById('remove');
 
 /*from previous ex*/
+//add an item to the bottom of the list
 button.addEventListener('click', () => {
     let listItem = document.createElement('li');
 
@@ -15,3 +17,10 @@ button.addEventListener('click', () => {
 })
 
 /*New code below*/
+//remove the last item from the list when the button is clicked
+remove.addEventListener('click', () => {
+    let lastItem = document.querySelector('li:last-child');
+    let list = document.getElementsByTagName('ul')[0];
+
+    list.removeChild(lastItem);
+})
