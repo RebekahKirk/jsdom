f81d7fb3c6b64901fd8364813704cbaf209f7661
RebekahKirk <rebekah.kirk@srm.com>
2020-05-06 15:18:30 +0100

    Updating file with challenge 8

--- 8 - Creating New Elements with JS/app.js
+++ 8 - Creating New Elements with JS/app.js
@@ -1,28 +1,22 @@
+//variables
 const input = document.getElementById('input');
 const button = document.getElementById('submit');
+let list = document.getElementsByTagName("ul")[0];
+const showhide = document.getElementById("showhide-btn");
 
-const showhidebtn = document.getElementById('showhide-btn');
-
-button.addEventListener('click', () => {
-    let listItem = document.createElement('li');
-    let list = document.getElementsByTagName('ul')[0];
+//create a list item when the person presses the submit button adding it to the
+button.addEventListener("click", ()=>{
+    let listItem = document.createElement("li");
     listItem.textContent = input.value;
     list.appendChild(listItem);
-    input.value = '';
 })
-
-//challenge cont'd
-//show/hide list
-//in console, type list.style to find object properties
-//then use list.style.display, it will show 'block' so you can show none
-showhidebtn.addEventListener("click", () => {
-    let list = document.getElementsByTagName('ul')[0];
-    if(list.style.display == 'none') {
-        list.style.display = 'block';
-        showhidebtn.textContent = 'hide';
+showhide.addEventListener("click", () => {
+    //let list=document.getElementsByTagName("ul")[0]; 
+    if (list.style.display == "none") {
+        list.style.display = "block";
+        showhide.textContent= "hide";
     } else {
-        list.style.display = 'none';
-        showhidebtn.textContent = 'show';
+        list.style.display = "none";
+        showhide.textContent = "show";
     }
 })
-//inspect the html you can then see inline style as display:"none" added in when button clicked
