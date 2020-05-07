f9e2e49f59d05d6ef4f1a16c905fe53408498f17
RebekahKirk <rebekah.kirk@srm.com>
2020-05-07 11:31:35 +0100

    Uploading with challenge 11 added

--- 12 - Event Object/app.js
+++ 12 - Event Object/app.js
@@ -23,3 +23,25 @@ removeBtn.addEventListener('click', () => {
 })
 
 /*New code below*/
+
+document.addEventListener("click", (event) =>
+{
+    console.log(event);
+    console.log(event.target);
+})
+
+//changes list items to uppercase when hovered over and back to lowercase when not however this looses the list when you hover over the bullet points.
+list.addEventListener("mouseover", (event) =>{
+    event.target.textContent = event.target.textContent.toUpperCase();})
+list.addEventListener("mouseout", (event) =>{
+    event.target.textContent = event.target.textContent.toLowerCase();})
+
+//the below code fixes the formating issue because it looks at the lI rather than the whole UL
+list.addEventListener("mouseover", (event)=>{
+    if (event.target.tagName == "LI"){
+        event.target.textContent = event.target.textContent.toUpperCase();}
+})
+list.addEventListener("mouseout", (event)=>{
+    if (event.target.tagName == "LI"){
+        event.target.textContent = event.target.textContent.toLowerCase();}
+})
